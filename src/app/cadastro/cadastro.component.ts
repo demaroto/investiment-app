df1529c02ca6f177a46ced98d8ea9a70440b5934
Wildemar Barbosa <demaroto69@gmail.com>
2018-06-15 05:10:48 -0300

    Update cadastro.component.ts

--- src/app/cadastro/cadastro.component.ts
+++ src/app/cadastro/cadastro.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
-
+/****
+* DESCRIÇÃO:Definição do componente cadastro
+* AUTOR: Wildemar Barbosa
+* ENTRADA:Seletor HTML, Template HTML e Folha de estilo CSS
+* SAIDA
+***/
 @Component({
   selector: 'app-cadastro',
   templateUrl: './cadastro.component.html',
